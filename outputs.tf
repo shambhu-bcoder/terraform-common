9e35df77721f4a641003d42f61ce71ed9fd583cc
shambhu_bcoder <shambhunath@bcoder.in>
2024-04-10 14:25:56 +0530

    code added

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "private_key_file" {
   value       = local_file.private_key.filename
   description = "pem file"
 }
+
+output "aws_instance" {
+  value = aws_instance.web_server.public_dns
+  description = "public dns"
+}
+
+output "aws_instance" {
+  value = aws_instance.web_server.public_ip
+  description = "public ip"
+}
--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "key_name" {
   description = "Enter or set a name of the key pair"
+  type        = string
+  default     = "mytuur-prod"
 }
 
 variable "mongodb_username" {
   description = "MongoDB admin username"
   type        = string
-  default     = "muytuur"
+  default     = "mytuur"
 }
 
 variable "mongodb_password" {
